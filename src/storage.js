004fbbb3bc72349601cacd0b69e872f4af641658
gitcommitshow <56937085+gitcommitshow@users.noreply.github.com>
2024-08-15 09:18:06 +0000

    feat: allow adding cla label manually

--- src/storage.js
+++ src/storage.js
@@ -6,34 +6,35 @@ import { PROJECT_ROOT_PATH } from "./helpers.js";
 const dbPath = process.env.DB_PATH || resolve(PROJECT_ROOT_PATH, "db.json");
 createFileIfMissing(dbPath);
 
-function createFileIfMissing(path){
+function createFileIfMissing(path) {
   try {
     // Try to open the file in read-only mode
-    const fd = fs.openSync(path, 'r');
+    const fd = fs.openSync(path, "r");
     fs.closeSync(fd);
-    console.log('DB file exists at '+dbPath);
+    console.log("DB file exists at " + dbPath);
   } catch (err) {
-    if (err.code === 'ENOENT') {
+    if (err.code === "ENOENT") {
       // If the file does not exist, create it
-      fs.writeFileSync(path, '[]', { flag: 'wx' }); // 'wx' flag ensures the file is created and not overwritten if it exists
-      console.log('DB file created at '+dbPath);
+      fs.writeFileSync(path, "[]", { flag: "wx" }); // 'wx' flag ensures the file is created and not overwritten if it exists
+      console.log("DB file created at " + dbPath);
     } else {
       // Some other error occurred
       console.error(err);
-      throw new Error("Failed to create the DB file at "+dbPath);
+      throw new Error("Failed to create the DB file at " + dbPath);
     }
   }
 }
 
 export const storage = {
   save(data) {
+    createFileIfMissing(dbPath);
     const currentData = JSON.parse(fs.readFileSync(dbPath, "utf8"));
     currentData.push(data);
     fs.writeFileSync(dbPath, JSON.stringify(currentData, null, 2));
   },
   get(filters) {
     const currentData = JSON.parse(fs.readFileSync(dbPath, "utf8"));
-    if(!filters) return currentData;
+    if (!filters) return currentData;
     return currentData.filter((item) => {
       for (const [key, value] of Object.entries(filters)) {
         if (item[key] !== value) {
